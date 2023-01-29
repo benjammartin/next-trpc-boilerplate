7dab3b9f9fb8ae1c6e46ed81b93cbc87e91c89b6
BMA <benjammartin@gmail.com>
2023-01-29 21:35:51 +0100

    ok

--- server/routers/example.ts
+++ server/routers/example.ts
@@ -1,10 +1,10 @@
-import useRequest from "@/utils/request";
+import request from "@/utils/request";
 import { procedure, router } from "server/trpc";
 
 const REQUEST_URL = "https://some-random-api.ml/animal/cat";
 export const exampleRouter = router({
   get: procedure.query(async () => {
-    const result = await useRequest<APIResponse>(REQUEST_URL);
+    const result = await request<APIResponse>(REQUEST_URL);
     return result;
   }),
 });
